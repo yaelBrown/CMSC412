2b1f870e084158eedc9af14e8bdc12e60bea2e33
Yael Brown <yaelrbrown@gmail.com>
2020-11-09 21:53:53 -0600

    Finished hw3

--- hw3/src/Homework3.java
+++ hw3/src/Homework3.java
@@ -1,7 +1,34 @@
 public class Homework3 {
-
     public static void main(String[] args) {
-        System.out.println("cookies");
+        for (int i = 0; i < 5; i++) {
+            String iteration = String.valueOf(i + 1);
+
+            NewThread nt1 = new NewThread(iteration);
+            NewThread nt2 = new NewThread(iteration);
+            NewThread nt3 = new NewThread(iteration);
+
+            nt1.run();
+            nt2.run();
+            nt3.run();
+        }
+    }
+}
+
+class NewThread implements Runnable {
+    String n;
+    Thread t;
+
+    NewThread(String t_name) {
+        n = t_name;
+        t = new Thread(this, t_name);
     }
 
+    public void run() {
+        try {
+            System.out.println("Thread " + t.getId() + " - iteration no. " + n + "\n");
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            System.out.println(n + " Interrupted: " + e);
+        }
+    }
 }
